e84a3c4e9372ee26bd5a214720cf03089ad8fc18
Animesh Manglik <amanglik@whalerockindustries.com>
2017-10-23 22:02:00 -0700

    Open all ports for Prod

--- src/run.py
+++ src/run.py
@@ -8,7 +8,7 @@ import os
 if __name__ == '__main__':
     if os.environ.get('WORK_ENV') == 'PROD':
         app = create_app(ProductionConfig)
-        app.run(port=5000, host="0.0.0.0", use_reloader=False)
+        app.run(host="0.0.0.0", use_reloader=False)
     else:
         app = create_app(DevelopmentConfig)
         app.run(port=5000, host="0.0.0.0", use_reloader=True)
